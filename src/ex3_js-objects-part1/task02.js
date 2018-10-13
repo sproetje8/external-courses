b273d77cd13c3b89f43aeb5e84c5cce3b80560bf
evelienfoubert <evelienfoubert@mail.ru>
2018-10-13 23:02:46 +0300

    fixed errors on task02.js

--- src/ex3_js-objects-part1/task02.js
+++ src/ex3_js-objects-part1/task02.js
@@ -1,22 +1,27 @@
 // ## Задание №2
 
 // Написать, функцию, которая принимает в качестве аргумента объект и выводит в консоль все ключи и значения свойств. Данная функция не должна возвращать значение.
+var obj1;
+var obj2;
 function showProps(obj){
     console.log(obj);
 }
-var obj1 = {a:1, b:'true'};
+obj1 = {a:1, b:'true'};
 showProps(obj1);
-var obj2 = {name:'David', lastName:'Malan', f:function(){
+obj2 = {name:'David', lastName:'Malan', f:function(){
     console.log(CS50)
 }
 };
 showProps(obj2);
 
 /*variant 2: shows in console each key and its value separately*/
+var obj3;
 function showKeyValue(obj){
     for (var key in obj){
-        console.log(key + " : " + obj[key]);
-    }
+        if (obj.hasOwnProperty(key)){
+            console.log(key + " : " + obj[key]);
+        }
+    }      
 }
-var obj1 = {name:"Peter", age:35, score:"2+5"};
-showKeyValue(obj1);
+obj3 = {name:"Peter", age:35, score:"2+5"};
+showKeyValue(obj3);
