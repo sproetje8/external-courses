559280306cbfa54ec7e72033b02d44ba9972bb52
evelienfoubert <evelienfoubert@mail.ru>
2018-10-14 18:31:37 +0300

    correct task05

--- src/ex3_js-objects-part1/task05.js
+++ src/ex3_js-objects-part1/task05.js
@@ -1,18 +1,18 @@
 // ## Задание №5
 
 // Написать функцию, которая принимает объект и создает его копию (новую ссылку) и возвращает скопированный объект
-var obj2;
 function copy(obj){
-    obj2 = {};
-    for (key in obj){
+    var obj2 = {};
+    for (var key in obj){
+        if (obj.hasOwnProperty(key)){
         obj2[key] = obj[key];
+        }
     } 
     return obj2;
 }
 obj = {d:5, b:2, r:'hello'};
 console.log(copy(obj));
-console.log(obj2);
-obj.a = 100;
-console.log(obj);
-console.log(obj2);
-console.log(copy(obj));
+console.log(obj === copy(obj)); //checking if the objects are the same
+obj.a = 100; //to change obj to compare with copy(obj)
+console.log(obj); 
+console.log(obj === copy(obj)); // to check if they have different references
