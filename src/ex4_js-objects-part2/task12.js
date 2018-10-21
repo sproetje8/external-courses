54e7d63f0c4cc1c418a5c3b81485b226b2c8812d
evelienfoubert <evelienfoubert@mail.ru>
2018-10-21 15:00:05 +0300

    correct task12

--- src/ex4_js-objects-part2/task12.js
+++ src/ex4_js-objects-part2/task12.js
@@ -3,11 +3,9 @@
 // Написать функцию, которая складывает два числа и результат округляется до 3го знака после запятой. Возвращаемый результат должен быть числом.
 function sum(a, b){
     var res = a + b;
-    if (typeof res === 'number'){
-    }
-    return res.toFixed(3);
+    return Number(res.toFixed(3));
 }
-console.log(sum(3.45, 5.36469));
+console.log(typeof sum(3.45, 5.36469));
 console.log(sum(0, 0));
-console.log(sum(3, 5));
+console.log(sum(3, 5.123));
 console.log(sum(4.1, 23.1));
