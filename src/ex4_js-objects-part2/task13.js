59fb888f73c6701cc40603c443c5333621910f0a
evelienfoubert <evelienfoubert@mail.ru>
2018-10-21 16:29:54 +0300

    add task 13

---
+++ src/ex4_js-objects-part2/task13.js
@@ -0,0 +1,7 @@
+// ## Задание 13
+
+// Написать функцию, которая возвращает случайное число в диапазоне [0; 100].
+function getRandomInt(){
+    return Math.floor(Math.random(100) * Math.floor(100));
+}
+console.log(getRandomInt());
