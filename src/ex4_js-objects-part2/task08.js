e4fd47e5bd97f68104fa1ad815cfd7d2ce57ef3a
evelienfoubert <evelienfoubert@mail.ru>
2018-10-21 10:32:34 +0300

    add task08

---
+++ src/ex4_js-objects-part2/task08.js
@@ -0,0 +1,17 @@
+// ## Задание 8
+
+// Написать функцию, которая принимает строку и возвращает данную строку, но в lowerCamelCase нотации.
+function toLowerCamelCase(str){
+    var words = str.toLowerCase().split(' ');
+    var i;
+    var adaptedChar;
+    for ( i = 1 ; i < words.length ; i++){
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    } 
+    return words.join('');
+}
+console.log(toLowerCamelCase('I was walking In The park'));
+console.log(toLowerCamelCase('Evelien Foubert'));
+console.log(toLowerCamelCase(''));
+console.log(toLowerCamelCase(' '));
+console.log(toLowerCamelCase('123'));
